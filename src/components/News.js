8b8ecaa932a057b818411a4f602f1d4c42be21c0
shivam-patel007 <patelshivam3132@gmail.com>
2026-07-09 15:40:12 +0530

    Adding Previous and Next Buttons to populate the news items and set pagesize = 20

--- src/components/News.js
+++ src/components/News.js
@@ -6,16 +6,36 @@ export class News extends Component {
   constructor() {
     super();
     this.state = {
-      articeles : [],
-      loading : false
+      articles : [],
+      loading : false,
+      page : 1
     }
   }
    
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/everything?q=india&sortBy=publishedAt&language=hi&apiKey=bafa763e12a4439989faf411c593def0";
+    let url = "https://newsapi.org/v2/everything?q=india&sortBy=publishedAt&language=hi&apiKey=bafa763e12a4439989faf411c593def0&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articeles: parsedData.articles})
+    this.setState({articles: parsedData.articles})
+  }
+  
+  handlePrevClick = async ()=>{
+    let url = `https://newsapi.org/v2/everything?q=india&sortBy=publishedAt&language=hi&apiKey=bafa763e12a4439989faf411c593def0&page=${this.state.page-1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+  handleNextClick = async ()=>{
+    let url = `https://newsapi.org/v2/everything?q=india&sortBy=publishedAt&language=hi&apiKey=bafa763e12a4439989faf411c593def0&page=${this.state.page+1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+    })
   }
 
   render() {
@@ -24,7 +44,7 @@ export class News extends Component {
         <div className="container my-4">
             <h2>NewsApp - Top Headlines</h2>
             <div className="row my-4" >
-              {this.state.articeles.map((element)=>{
+              {this.state.articles.map((element)=>{
                 return <div className="col-md-4" key={element.url}>
                   <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl ={element.urlToImage} newsUrl={element.url}/>
                 </div>
@@ -32,6 +52,10 @@ export class News extends Component {
               )}
             </div>
 
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button  disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
